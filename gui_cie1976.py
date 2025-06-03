fd1055a2dd0dfb8a8042b3bff955827379da60fe
OscarYeh <oscaryeh.tw@gmail.com>
2025-06-03 17:33:43 +0800

    Add live cursor coordinates plugin

--- gui_cie1976.py
+++ gui_cie1976.py
@@ -10,6 +10,96 @@ from mpld3 import plugins
 import numpy as np
 from matplotlib.path import Path
 
+class TickStylePlugin(plugins.PluginBase):
+    JAVASCRIPT = """
+    mpld3.register_plugin("tickstyle", TickStylePlugin);
+    TickStylePlugin.prototype = Object.create(mpld3.Plugin.prototype);
+    TickStylePlugin.prototype.constructor = TickStylePlugin;
+    function TickStylePlugin(fig, props){
+        mpld3.Plugin.call(this, fig, props);
+        this.fig = fig;
+    }
+    TickStylePlugin.prototype.draw = function(){
+        d3.selectAll("g.tick line").style("stroke-dasharray", "2,2")
+                                   .style("stroke-width", "0.5")
+                                   .style("stroke", "gray");
+    };
+    """
+    def __init__(self):
+        self.dict_ = {"type": "tickstyle"}
+
+class CrosshairPlugin(plugins.PluginBase):
+    JAVASCRIPT = """
+    mpld3.register_plugin("crosshair", CrosshairPlugin);
+    CrosshairPlugin.prototype = Object.create(mpld3.Plugin.prototype);
+    CrosshairPlugin.prototype.constructor = CrosshairPlugin;
+
+    function CrosshairPlugin(fig, props){
+        mpld3.Plugin.call(this, fig, props);
+        this.fig = fig;
+        this.ax = fig.axes[0];
+    }
+
+    CrosshairPlugin.prototype.draw = function(){
+        var fig = this.fig;
+        var ax = this.ax;
+
+        // var svg = d3.select(fig.canvas);
+        var svg = d3.select("svg.mpld3-figure");
+
+        var width = +svg.attr("width");
+        var height = +svg.attr("height");
+
+        this.vline = svg.append("line")
+            .attr("stroke", "gray")
+            .attr("stroke-width", 1)
+            .attr("pointer-events", "none")
+            .style("display", "none");
+
+        this.hline = svg.append("line")
+            .attr("stroke", "gray")
+            .attr("stroke-width", 1)
+            .attr("pointer-events", "none")
+            .style("display", "none");
+
+        var vline = this.vline;
+        var hline = this.hline;
+
+        svg.on("mousemove", function(event){
+            var coords = d3.mouse(this);
+            var x = ax.x.invert(coords[0]);
+            var y = ax.y.invert(coords[1]);
+
+            if(x < 0 || x > 0.7 || y < 0 || y > 0.7){
+                vline.style("display", "none");
+                hline.style("display", "none");
+                return;
+            }
+
+            var px = ax.x(x);
+            var py = ax.y(y);
+
+            vline
+                .attr("x1", px).attr("y1", 0)
+                .attr("x2", px).attr("y2", height)
+                .style("display", null);
+
+            hline
+                .attr("x1", 0).attr("y1", py)
+                .attr("x2", width).attr("y2", py)
+                .style("display", null);
+        });
+
+        svg.on("mouseleave", function(){
+            vline.style("display", "none");
+            hline.style("display", "none");
+        });
+    };
+    """
+
+    def __init__(self):
+        self.dict_ = {"type": "crosshair"}
+
 class MousePositionPlugin(plugins.PluginBase):
     """Plugin to display u′ and v′ coordinates on hover (D3 v3 compatible)."""
     JAVASCRIPT = """
@@ -40,14 +130,24 @@ class MousePositionPlugin(plugins.PluginBase):
         document.body.appendChild(coordsDiv);
 
         fig.canvas.on("mousemove", function(){
-            var coords = d3.mouse(this);
-            var x = ax.x.invert(coords[0]);
-            var y = ax.y.invert(coords[1]);
-            coordsDiv.innerHTML = "u′ = " + x.toFixed(4) + ", v′ = " + y.toFixed(4);
+            var rect = this.getBoundingClientRect();
+            var mouseX = d3.event.clientX - rect.left;
+            var mouseY = d3.event.clientY - rect.top;
+
+            var x = ax.x.invert(mouseX - ax.position[0]);
+            var y = ax.y.invert(mouseY - ax.position[1]);
+
+            if (x >= 0 && x <= 0.7 && y >= 0 && y <= 0.7) {
+                coordsDiv.innerHTML = "u′ = " + x.toFixed(4) + ", v′ = " + y.toFixed(4);
+                coordsDiv.style.display = "block";
+            } else {
+                coordsDiv.style.display = "none";
+            }
         });
 
         fig.canvas.on("mouseleave", function(){
             coordsDiv.innerHTML = "u′ = ---, v′ = ---";
+            coordsDiv.style.display = "block";
         });
     };
     """
@@ -89,7 +189,7 @@ colour.plotting.plot_chromaticity_diagram_CIE1976UCS(standalone=False, axes=ax)
 # === Step 5: Draw ellipses ===
 for u, v, major, minor, angle in ellipses:
     ellipse = Ellipse(xy=(u, v), width=major, height=minor, angle=angle,
-                      edgecolor='black', facecolor='none', linewidth=1)
+                      edgecolor='black', facecolor='none', linewidth=0.5)
     ax.add_patch(ellipse)
 
 plt.axis([0, 0.7, 0, 0.7])
@@ -99,12 +199,14 @@ plt.title("CIE 1976 Chromaticity Diagram")
 plt.tight_layout()
 
 # === Step 6: Add live cursor coordinates plugin ===
-
-# === Step 7: Save HTML ===
 fig = plt.gcf()
 plugins.connect(fig, MousePositionPlugin())
+plugins.connect(fig, TickStylePlugin())
+#plugins.connect(fig, CrosshairPlugin())
 
-html_path = xlsx_path.replace('.xlsx', '_cie1976.html')
+# === Step 7: Save HTML ===
+#html_path = xlsx_path.replace('.xlsx', '_cie1976.html')
+html_path = 'cie1976_out.html'
 with open(html_path, "w", encoding="utf-8") as f:
     f.write(mpld3.fig_to_html(fig))
 
